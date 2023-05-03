65fb6203e9b89b847d1361065808c75e0c8ccb8d
Mongelard Fabien <fabien.mongelard@gmail.com>
2023-05-03 15:05:05 +0200

    added file "scratch" that holds experimental code

---
+++ scratch.R
@@ -0,0 +1,14 @@
+
+df_only_shNS <- filter( loaded_table, Cell=='shNS #1')
+
+loaded_table_mutated <- mutate(loaded_table, Cell_Dox=paste(Cell,Dox),sep="_")
+plot_c <- ggplot(loaded_table_mutated, aes(NCL, fill=Cell_Dox)) + 
+  geom_histogram(binwidth = 100) +
+  scale_fill_brewer(palette = "Paired") 
+
+plot_d <- ggplot(df_only_shNS, aes(NCL, fill=Dox)) +
+  geom_histogram(binwidth = 250) +
+  scale_fill_brewer(palette = "Paired")
+plot_b
+plot_c
+plot_d
