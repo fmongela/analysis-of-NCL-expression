93910b9d5200ec3c666abf6962e3a1a9947c75bb
Mongelard Fabien <fabien.mongelard@gmail.com>
2023-05-05 15:09:51 +0200

    ee

--- #3247_NCL_intensity.R
+++ #3247_NCL_intensity.R
@@ -4,12 +4,21 @@
 # \_)(_/(____/(____)  (__/ (_/        \_)__) \___)\____/
   
 rm(list=ls())
+plot_list <- list()
 my_packs <- c("readr","ggplot2","tidyverse","rstatix","ggprism",
-              "ggthemes","scales","grid","RColorBrewer")
+              "ggthemes","scales","gridExtra","RColorBrewer")
 lapply(my_packs, library, character.only = TRUE)
 theme_set(theme_bw())
-chemin <- "D:/smyl/Labo/projects/KD NCL HCT116/Analyses HCS/id #3247/NCL intensity/compil/IF shRNA HCT116 clones 230418__2023-04-19T18_04_21-Measurement 1b/Evaluation6/Objects_Population - Nuclei Selected.txt"
-loaded_table <- read_tsv(chemin, skip = 9, col_select = c("Cell Type", "Dox", "Nuclei Selected - NCL intensity Mean"))
+
+pathFileList <- list("D:/smyl/Labo/projects/KD NCL HCT116/Analyses HCS/id #3247/NCL intensity/compil/IF shRNA HCT116 clones 230418__2023-04-19T18_04_21-Measurement 1b/Evaluation6/Objects_Population - Nuclei Selected.txt",
+"D:\\smyl\\Labo\\projects\\KD NCL HCT116\\Analyses HCS\\id #3247\\NCL intensity\\compil\\#3247 G1H12 B23 restarted with 25% laser__2023-04-21T17_34_05-Measurement 1b\\Evaluation1\\Objects_Population - Nuclei Selected.txt",
+"D:\\smyl\\Labo\\projects\\KD NCL HCT116\\Analyses HCS\\id #3247\\NCL intensity\\compil\\#3247__2023-04-21T14_33_50-Measurement 1b\\Evaluation1\\Objects_Population - Nuclei Selected.txt")
+
+BoPlot <- function(pathFile){
+
+loaded_table <- read_tsv(pathFile, skip = 9, col_select = c("Cell Type", "Dox", "Nuclei Selected - NCL intensity Mean","Antibody"))
+#get antibody name
+antibody <<- loaded_table %>% slice(11) %>% pull("Antibody") %>% as.character()
 #rename columns
 loaded_table <- loaded_table %>% rename('Cell'=`Cell Type`) %>% rename('NCL'=`Nuclei Selected - NCL intensity Mean`)
 #reorder cell lines and dox treatments for plotting
@@ -17,13 +26,18 @@ loaded_table$Cell <-factor(loaded_table$Cell,levels=c("shNS #1", "shNS #2", "sh2
 loaded_table$Dox <-factor(loaded_table$Dox,levels=c("noDox", "+Dox"))
 loaded_table <- unite(loaded_table, Cell, Dox, col = "Cell_Dox", sep="_", remove = FALSE)
 
-plot_a <- ggplot(loaded_table, aes(x=Cell, y=NCL, fill=Dox))
-plot_b <- plot_a +  geom_violin(position=position_dodge(0.75)) +
-  scale_fill_brewer(palette = "Paired")
+plot_a <- ggplot(loaded_table, aes(x=Cell, y=NCL, fill=Dox)) +
+  geom_violin(position=position_dodge(0.75)) +
+  scale_fill_brewer(palette = "Paired") +
+  labs(title = antibody)
+return(plot_a)
+}
 
-plot_b
+plots_list <- lapply(pathFileList, BoPlot)
 
-
-
-aov.model <- aov(loaded_table$NCL ~ loaded_table$Cell_Dox)
+# Arrange the plots in a grid using gridExtra
+grid.arrange(
+  grobs = lapply(plots_list, ggplotGrob),
+  ncol = 1
+)
 
